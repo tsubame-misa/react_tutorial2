2d5d1bf8b48384e9889b2274b9ec53a24d11df5d
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-26 00:29:09 +0900

    update

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,10 @@ import { App } from "./App.js";
 
 render(
   <Auth0Provider
-    //domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    //clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    clientId="Y3xd6gto4h0LECF27CZ9q1cMXoy1vmaq"
-    domain="dev-7o4t1t5n.us.auth0.com"
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    //clientId="Y3xd6gto4h0LECF27CZ9q1cMXoy1vmaq"
+    //domain="dev-7o4t1t5n.us.auth0.com"
     redirectUri={window.location.origin}
   >
     <App />
