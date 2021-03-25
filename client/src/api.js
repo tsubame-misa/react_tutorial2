8839bd5face4ef77adfdfe42d607cd1ccfb014b9
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-25 21:12:29 +0900

    update

--- client/src/api.js
+++ client/src/api.js
@@ -1,19 +1,20 @@
 async function request(path, options = {}) {
-    const url = `${process.env.REACT_APP_API_ORIGIN}${path}`;
-    const response = await fetch(url, options);
-    return response.json();
-  }
-  
-  export async function getRestaurants(arg = {}) {
-    const params = new URLSearchParams(arg);
-    return request(`/restaurants?${params.toString()}`);
-  }
-  
-  export async function getRestaurant(restaurantId) {
-    return request(`/restaurants/${restaurantId}`);
-  }
-  
-  export async function getRestaurantReviews(restaurantId, arg = {}) {
-    const params = new URLSearchParams(arg);
-    return request(`/restaurants/${restaurantId}/reviews?${params.toString()}`);
-  }
+  const url = `${process.env.REACT_APP_API_ORIGIN}${path}`;
+  console.log(url);
+  const response = await fetch(url, options);
+  return response.json();
+}
+
+export async function getRestaurants(arg = {}) {
+  const params = new URLSearchParams(arg);
+  return request(`/restaurants?${params.toString()}`);
+}
+
+export async function getRestaurant(restaurantId) {
+  return request(`/restaurants/${restaurantId}`);
+}
+
+export async function getRestaurantReviews(restaurantId, arg = {}) {
+  const params = new URLSearchParams(arg);
+  return request(`/restaurants/${restaurantId}/reviews?${params.toString()}`);
+}
